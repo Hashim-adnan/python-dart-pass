8933ea05806b19c06a47dd3c1725fb308646436d
Hashim adnan <hashim.alnuimi@gmail.com>
2022-04-12 19:36:28 +0300

    Create Question1.dart

---
+++ dart-pass/Question1.dart
@@ -0,0 +1,7 @@
+main() {
+  const hour = DataTime.now().hour;
+  print(hour);
+}
+
+//const is a variable type that can only be used with constant values 
+//like the (pi π) for example or any other values that we don't want it to be changed

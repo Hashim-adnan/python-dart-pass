277cdc7caff88f07a723e08aa4eeaf35c1ef2f60
Hashim adnan <hashim.alnuimi@gmail.com>
2022-04-12 19:41:48 +0300

    Update Question2.dart

--- dart-pass/Question2.dart
+++ dart-pass/Question2.dart
@@ -1,15 +1,90 @@
-def EvenOddChecker():
-    list = []
-    SizeOfList = int(input("Enter x value: "))
-    if SizeOfList > 0 and SizeOfList <= 10:
-        
-        for x in range(SizeOfList):
-            NumIn = int(input())
-            list.append(NumIn)
-            
-        for placeinlist in range(0, len(list)):
-                if list[placeinlist] % 2 == 0:
-                    print(list[placeinlist],'is even')
-                else:
-                    print(list[placeinlist],'is odd')
-EvenOddChecker()
+
+abstract class Animal {
+
+  void printName();
+
+  void printSound();
+
+}
+
+ 
+
+class Dog extends Animal {
+
+  void printName() {
+
+    print("Dog");
+
+  }
+
+ 
+
+  void printSound() {
+
+    print("Woof");
+
+  }
+
+}
+
+ 
+
+class Cat extends Animal {
+
+  void printName() {
+
+    print("Cat");
+
+  }
+
+ 
+
+  void printSound() {
+
+    print("Meaw");
+
+  }
+
+}
+
+ 
+
+class Cow extends Animal {
+
+  void printName() {
+
+    print("Cow");
+
+  }
+
+ 
+
+  void printSound() {
+
+    print("Moo");
+
+  }
+
+}
+
+ 
+
+void main() {
+
+  var c = Cat(), d = Dog(), w = Cow();
+
+  c.printName();
+
+  c.printSound();
+
+  d.printName();
+
+  d.printSound();
+
+  w.printName();
+
+  w.printSound();
+
+}
+
+
